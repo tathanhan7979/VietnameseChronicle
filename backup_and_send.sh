ee2d6786c02206952d1fd9433e9230e4d50b504e
Tạ An <103182402+tathanhan7979@users.noreply.github.com>
2025-05-06 06:58:16 +0000

    update

--- backup_and_send.sh
+++ backup_and_send.sh
@@ -7,8 +7,8 @@ DB_PASSWORD="Hihihaha123!"
 BACKUP_FILE="/root/backup_$(date +%F).sql"
 
 # === Thông tin cấu hình Telegram ===
-BOT_TOKEN="YOUR_BOT_TOKEN"            # ← Thay bằng token của bot
-CHAT_ID="YOUR_CHAT_ID"                # ← Thay bằng chat ID của bạn
+BOT_TOKEN="7739857322:AAGjqURU0HmrBC7Xw6sRMLPoyw5LIEcU0vE"   # ← Thay bằng token của bot
+CHAT_ID="-1002513693228"                # ← Thay bằng chat ID của bạn
 
 # === Export password để pg_dump không hỏi ===
 export PGPASSWORD=$DB_PASSWORD
